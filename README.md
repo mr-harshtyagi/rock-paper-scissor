9feb43e434827bacf4c5ceb10899e2ea720048f3
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-02-18 06:50:22 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stone-paper-scissor
+It's a Stone paper Scissor game.
+Click on the link to play now: https://yshv-2022.github.io/stone-paper-scissor/
---
+++ index.js
@@ -0,0 +1,100 @@
+var userScore = 0;
+var computerScore = 0;
+var userScore = document.getElementById("user-score");
+var computerScore = document.getElementById("computer-score");
+var result = document.querySelector(".result-message");
+var initial =document.querySelector(".initial");
+var final =document.querySelector(".final");
+var userResult=document.getElementById("user-move");
+var computerResult=document.getElementById("computer-move");
+var message=document.getElementById("message");
+var u=0;
+var c=0;
+
+for (var i = 0; i < 3; i++) {
+  document.querySelectorAll(".images")[i].addEventListener("click", function() {
+
+    var computerMove = Math.ceil(Math.random() * 3);
+
+    switch (computerMove) {
+      case 1:
+        computerMove="stone";
+        break;
+      case 2:
+        computerMove="paper";
+        break;
+      case 3:
+        computerMove="scissor";
+        break;
+
+      default:
+         result.innerHTML="ERROR 404";
+    }
+  var userMove=this.id;
+    if(userMove=="stone"){
+       if(computerMove=="paper"){
+          result.innerHTML="YOU LOSE😢";
+          c++;
+          computerScore.innerHTML=c;
+       }
+       if(computerMove=="scissor"){
+          result.innerHTML="YOU WON😍";
+          u++;
+          userScore.innerHTML=u;
+       }
+       if(computerMove=="stone"){
+          result.innerHTML="It's a DRAW";
+
+       }
+
+    }
+    if(userMove=="paper"){
+      if(computerMove=="scissor"){
+         result.innerHTML="YOU LOSE😢";
+         c++;
+         computerScore.innerHTML=c;
+      }
+      if(computerMove=="stone"){
+         result.innerHTML="YOU WON😍";
+         u++;
+         userScore.innerHTML=u;
+      }
+      if(computerMove=="paper"){
+         result.innerHTML="OOPS!! It's a DRAW";
+
+      }
+
+    }
+    if(userMove=="scissor"){
+      if(computerMove=="stone"){
+         result.innerHTML="YOU LOSE😢";
+         c++;
+         computerScore.innerHTML=c;
+      }
+      if(computerMove=="paper"){
+         result.innerHTML="YOU WON😍";
+         u++;
+         userScore.innerHTML=u;
+      }
+      if(computerMove=="scissor"){
+         result.innerHTML="OOPS!! It's a DRAW";
+
+      }
+
+    }
+    userResult.src="images/" +userMove +".png";
+    computerResult.src="images/" +computerMove +".png";
+    initial.style.display="none";
+    final.style.display="flex";
+    message.innerHTML="FINAL RESULT";
+
+
+    setTimeout(()=>{
+      final.style.display="none";
+      initial.style.display="flex";
+      message.innerHTML= "CHOOSE AGAIN TO PLAY FURTHER";
+    },5000);
+
+
+  });
+}
